35458d7044249049c590df1204d7e5546027f497
Mahmud Un Nabi <unmahmud@gmail.com>
2024-10-18 01:19:04 +0600

    practice

---
+++ C++ Practice ICPC Assiut University Community/Loop/contests/A. Timon and Pumbaa.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    int a,b;
+    cin>>a>>b;
+    int cont =0;
+    for(int i=b;i<a;i++)
+    {
+        cont++;
+    }
+    cout<<cont;
+}
