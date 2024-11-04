b6bae67b5da25b8203b4d91ec1c3e8da72ff1ab4
Mahmud Un Nabi <unmahmud@gmail.com>
2024-11-04 10:34:44 +0600

    practice

---
+++ C++ Practice ICPC Assiut University Community/Function/A. Add.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int add(long long int a,long long int b)
+{
+    long long int sum = a+b;
+    return sum;
+}
+int main ()
+{
+    long long int a,b;
+    cin>>a>>b;
+    long long int result = add(a,b);
+    cout<<result;
+
+}
