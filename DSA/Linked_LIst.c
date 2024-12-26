07f2a4518e1e6af07c9ccf7036bb8d9583085a6f
Mahmud394 <unmahmud@gmail.com>
2024-12-26 23:58:47 +0600

    practice

--- DSA/Linked_LIst.c
+++ DSA/Linked_LIst.c
@@ -6,9 +6,10 @@ struct Node {
     struct Node* next;
 };
 
+
 struct Node* createNode(int data) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = data;/
+    newNode->data = data;
     newNode->next = NULL;
     return newNode;
 }
