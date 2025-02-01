9617154644f46ba2e79327c4154d0f2193a45413
Mahmud394 <unmahmud@gmail.com>
2025-02-01 22:44:14 +0600

    practice

---
+++ C++ Practice ICPC Assiut University Community/Recursion/F. Print Even Indices.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printEvenIndicesReversed(int A[], int index) 
+{
+    if (index < 0) 
+        return; 
+    
+    cout << A[index] << " "; 
+    printEvenIndicesReversed(A, index - 2); 
+}
+
+int main() {
+    int N;
+    cin >> N;
+    int A[N];
+    
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+    
+    int startIndex;
+    if (N % 2 == 0) {
+        startIndex = N - 2;
+    } else {
+        startIndex = N - 1;
+    }
+    printEvenIndicesReversed(A, startIndex); 
+    
+    return 0;
+}
